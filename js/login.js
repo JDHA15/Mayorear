7e41196284fa228fbd12bd72c7c528eec15751c4
JDHA15 <josue.hernandez2026@hilasal.superate.org.sv>
2024-10-11 17:38:50 -0600

    se agrega funcion a login y a registrarse

--- js/login.js
+++ js/login.js
@@ -7,21 +7,25 @@ guardar.addEventListener('submit', async(e) =>{
     const password = guardar.password.value;
 
     const response = await fetch('https://api.sheetbest.com/sheets/07b8c941-318f-4e31-aedd-9e625cdd2b38', {
-        method: 'POST',
+        method: 'GET',
         mode: 'cors',
         headers:{
             'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "correo": email, 
-            "password": password
-        })
+        }
     })
 
     if(response.ok){
-        alert("Se ha registrado con exito");
+      const data = await response.json();
+
+      const user = data.find(user => user.correo === email && user.password === password);
+
+      if (user) {
+        alert("Tus datos son correctos, ya puedes ingresar")
         window.location.href = '../principal.html';
+      } else {
+        alert("Error, datos erroneos, vuelve a intentar")
+      }
     }else{
-        alert("Hubo un problema al enviar los datos")
+        alert("Hubo problemas técnicos")
     }
 })
